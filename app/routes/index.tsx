e832cac8849e80b000924fb47403c92b4d140f0e
Tinodaishe Tembo <synthmancer@gmail.com>
2024-05-22 11:23:07 +0200

    add from actions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,40 +1,82 @@
+import { type ActionArgs, redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export async function action({ request }: ActionArgs) {
+	console.log(request);
+	let formData = await request.formData();
+	let json = Object.fromEntries(formData);
+
+	console.log(json);
+
+	return redirect("/");
+}
+
 export default function Index() {
-  return (
-    <div className="p-10">
-      <h1 className="text-5xl">Work Journal</h1>
-      <p className="mt-2 text-lg text-gray-400">
-        Learnings and doings. Updated weekly.
-      </p>
+	return (
+		<div className="p-10">
+			<h1 className="text-5xl">Dojo</h1>
+			<p className="mt-2 text-lg text-gray-400">
+				Learnings and doings. Updated Weekly.
+			</p>
 
-      <div className="mt-6">
-        <p className="font-bold">
-          Week of February 20<sup>th</sup>
-        </p>
+			<div className="my-8 border p-3">
+				<Form method="post">
+					<p className="italic">Create an entry</p>
+					<div className="mt-4">
+						<input
+							type="date"
+							name="date"
+							className="text-gray-700"
+						/>
+					</div>
+					<div className="mt-2 space-x-6">
+						<label>
+							<input
+								className="mr-1"
+								type="radio"
+								name="category"
+								value="work"
+							/>
+							Work
+						</label>
+						<label>
+							<input
+								className="mr-1"
+								type="radio"
+								name="category"
+								value="learning"
+							/>
+							Learning
+						</label>
+						<label>
+							<input
+								className="mr-1"
+								type="radio"
+								name="category"
+								value="interesting-thing"
+							/>
+							Interesting thing
+						</label>
+					</div>
 
-        <div className="mt-3 space-y-4">
-          <div>
-            <p>Work</p>
-            <ul className="ml-8 list-disc">
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
-          </div>
-          <div>
-            <p>Learnings</p>
-            <ul className="ml-8 list-disc">
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
-          </div>
-          <div>
-            <p>Interesting things</p>
-            <ul className="ml-8 list-disc">
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="mt-2">
+						<textarea
+							name="text"
+							className="w-full text-gray-700"
+							placeholder="Write your entry..."
+						/>
+					</div>
+
+					<div className="mt-1 text-right">
+						<button
+							className="bg-blue-500 px-4 py-1 font-medium text-white"
+							type="submit"
+						>
+							Save
+						</button>
+					</div>
+				</Form>
+			</div>
+		</div>
+	);
 }
